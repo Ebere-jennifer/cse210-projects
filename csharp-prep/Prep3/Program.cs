3bfa9c000e0681ec4fd9d4135e0b5c5cd8c41082
Ebere-jennifer <ebe22004@byui.edu>
2024-02-29 15:22:46 +0100

    Week 1 Prove: Developer Programming Assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // For Core Requirement 1 and 2 whwre the user has to guess a number
+        // Console.WriteLine("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        // For Core Requirement 3 where we generate a random number
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        int totalGuesses = 0;
+
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            totalGuesses++;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"Congratulations! You guessed it! ");
+                Console.WriteLine($"It took you {totalGuesses} guesses.");
+                Console.Write("Do you want to play again? (yes/no): ");
+
+                string playAgain = Console.ReadLine().ToLower();
+
+                if (playAgain != "yes")
+                {
+                    Console.WriteLine("Thank you for playing. Goodbye! ");
+                    return;
+                }
+                else 
+                {
+                    totalGuesses = 0;
+                    magicNumber = randomGenerator.Next(1, 101);
+                }
+            }
+        }
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List <int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number: ");
+            string userResponse  = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            //If user number is not equal to zero
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        
+       // 1. Compute the sum, or total, of the numbers in the list.
+       int sum = 0;
+       foreach (int number in numbers)
+       {
+        sum += number;
+       }
+
+       Console.WriteLine($"The sum is: {sum}");
+
+       // 2. Compute the average of the numbers in the list.
+       float average = ((float)sum) / numbers.Count;
+       Console.WriteLine($"The average is: {average}");
+
+       // 3. Find the maximum, or largest, number in the list
+       int max = numbers[0];
+
+       foreach (int number in numbers)
+       {
+        if (number > max)
+        {
+            // if this number is greater than the max, we have found the new max!
+            max = number;
+        }
+       }
+
+       Console.WriteLine($"The maximum or largest number is: {max}");
     }
 }
