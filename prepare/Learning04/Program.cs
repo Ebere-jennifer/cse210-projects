7404ff46d2ce861c9644450c86dcc486f2a283f6
Ebere-jennifer <ebe22004@byui.edu>
2024-03-19 14:08:44 +0100

    Learning Activity Week 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // Create a base "Assignment" object
+        Assignment a1 = new Assignment("Jennifer Ebere", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+
+        // Create the MathAssignment class
+        MathAssignment a2 = new MathAssignment("Jennifer Ebere", "Fractions", "7.3", "8-19");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        // Create the WritingAssignment class
+        WritingAssignment a3 = new WritingAssignment("Jennifer Ebere", "European History", "The Causes of World War II");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
